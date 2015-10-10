dbbf18cd3e290eedd8edb72c22ed268f2af03dde
Adrian <andarms@outlook.com>
2015-10-09 23:47:29 -0500

    Add Stories app

--- scribimus/scribimus/urls.py
+++ scribimus/scribimus/urls.py
@@ -17,5 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^story/', include('apps.stories.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
