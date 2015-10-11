f52d1ab5b7ad0d484df1aea53acff89d7827bed4
Adrian <andarms@outlook.com>
2015-10-11 16:51:51 -0500

    Create simple CRUD
    delete view added but only works on POST request
    TODO make deletion via ajax

--- src/apps/stories/urls.py
+++ src/apps/stories/urls.py
@@ -5,4 +5,11 @@ from . import views
 urlpatterns = [
     url(r'^$', views.StoryListView.as_view(), name='list'),
     url(r'^create/', views.StoryCreateView.as_view(), name='create'),
+    url(r'^edit/(?P<pk>\d+)$', views.StoryUpdateView.as_view(), name='edit'),
+    url(
+        r'^delete/(?P<pk>\d+)$',
+        views.StoryDeleteView.as_view(),
+        name='delete'
+    ),
+    url(r'^(?P<pk>\d+)/$', views.StoryDetailView.as_view(), name='detail'),
 ]
--- src/apps/stories/views.py
+++ src/apps/stories/views.py
@@ -1,21 +1,37 @@
-from django.shortcuts import render
-from django.views.generic import CreateView, ListView
+from django.core.urlresolvers import reverse_lazy
+from django.views.generic import CreateView, ListView, UpdateView,\
+    DeleteView, DetailView
 
 
 from .models import Story
 
 
-def home(request):
-    return render(request, "home.html", {})
-
-
 class StoryListView(ListView):
+    context_object_name = "stories"
+    model = Story
     template_name = "stories/list.html"
+
+
+class StoryDetailView(DetailView):
+    context_object_name = "story"
+    template_name = "stories/story.html"
     model = Story
 
 
 class StoryCreateView(CreateView):
-    template_name = "stories/create.html"
+    template_name = "stories/form.html"
     model = Story
     fields = ['name', 'description', 'category']
-    success_url = '/story/'
+    success_url = reverse_lazy('story:list')
+
+
+class StoryUpdateView(UpdateView):
+    template_name = "stories/form.html"
+    model = Story
+    fields = ['name', 'description', 'category']
+    success_url = reverse_lazy('story:list')
+
+
+class StoryDeleteView(DeleteView):
+    model = Story
+    success_url = reverse_lazy('story:list')
