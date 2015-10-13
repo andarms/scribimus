1605f642966ed6895fcc0ed73a3197729ae97588
Adrian <andarms@outlook.com>
2015-10-12 20:37:41 -0500

    Create simple profile page

--- src/apps/members/urls.py
+++ src/apps/members/urls.py
@@ -4,4 +4,9 @@ from . import views
 
 urlpatterns = [
     url(r'^create/', views.MemberCreateView.as_view(), name='create'),
+    url(
+        r'^([\w-]+)/',
+        views.MemberDetailView.as_view(),
+        name='detail'
+    ),
 ]
--- src/apps/members/views.py
+++ src/apps/members/views.py
@@ -2,11 +2,12 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse_lazy
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
-from django.views.generic import CreateView
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import CreateView, ListView
 
 
 from .forms import UserCreateForm
+from apps.stories.models import Story
 
 
 def login_view(request):
@@ -42,3 +43,12 @@ class MemberCreateView(CreateView):
     template_name = 'members/form.html'
     form_class = UserCreateForm
     success_url = reverse_lazy('story:list')
+
+
+class MemberDetailView(ListView):
+    context_object_name = "stories"
+    template_name = "members/profile.html"
+
+    def get_queryset(self):
+        self.author = get_object_or_404(User, username=self.args[0])
+        return Story.objects.filter(author=self.author.member)
